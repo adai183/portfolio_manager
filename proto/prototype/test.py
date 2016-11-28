ad5bc6adaa5f2374db84ca4129eed09f0d85239f
adai183 <andreas.daiminger@gmail.com>
2016-11-28 15:54:37 +0100

    initial commit

---
+++ proto/prototype/test.py
@@ -0,0 +1,51 @@
+# QSTK Imports
+import QSTK.qstktools.YahooDataPull as yahoo
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+# Third Party Imports
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+    ''' Main Function'''
+    # get latest stock data for portfolio
+    yahoo.main(path='../../QSData/Yahoo/', symbolsPath='../../QSData/Yahoo/Lists/myPortfolio.txt')
+
+    # Creating an object of DataAccess Class
+    c_dataobj = da.DataAccess('Yahoo')
+
+    # Getting a list of symbols from Lists
+    ls_symbols = c_dataobj.get_symbols_from_list('myPortfolio')
+    print "Symbols from the list : ", ls_symbols
+
+
+    ls_syms_toread = ['FB','GOOGL', 'INFI', 'MDSY', 'NVDA', 'TSLA']
+
+    # List of TimeStamps to read
+    # set start date
+    # set start date to now
+    now = dt.datetime.now()
+    startdate = dt.datetime(now.year, now.month, now.day, 16)
+
+    if now.hour < 16:
+        startdate = dt.datetime(now.year, now.month, now.day, 16) - dt.timedelta(days=1)
+
+    backtrace_range = 30
+
+    ldt_timestamps =  [startdate - dt.timedelta(days=i) for i in range(0, backtrace_range)]
+    ldt_timestamps.reverse()
+
+    # Reading the data
+    # By default it'll read data from the default data provided,
+    # But a path can be provided using either an environment variable or
+    # as a prarameter.
+    df_close = c_dataobj.get_data(ldt_timestamps, ls_syms_toread, "close").dropna()
+
+    print df_close
+
+
+if __name__ == '__main__':
+    main()
